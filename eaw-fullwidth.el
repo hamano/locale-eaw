5c4e5929a248764ef34595d840be044b1bc3cc27
HAMANO Tsukasa <code@cuspy.org>
2014-09-18 00:02:59 +0900

    update eaw-fullwidth.el

--- eaw-fullwidth.el
+++ eaw-fullwidth.el
@@ -1,3 +1,5 @@
+(provide 'eaw-fullwidth)
+
 (setq east-asian-ambiguous '(
   #x00A1 ; INVERTED EXCLAMATION MARK
   #x00A4 ; CURRENCY SIGN
@@ -695,3 +697,16 @@
   #x277F ; DINGBAT NEGATIVE CIRCLED NUMBER TEN
   #xFFFD ; REPLACEMENT CHARACTER
 ))
+
+(when (= emacs-major-version 22)
+  (utf-translate-cjk-set-unicode-range east-asian-ambiguous))
+
+(when (>= emacs-major-version 23)
+  (while (char-table-parent char-width-table)
+    (setq char-width-table (char-table-parent char-width-table)))
+  (let ((table (make-char-table nil)))
+    (mapc (lambda (range) (set-char-table-range table range 2))
+          east-asian-ambiguous)
+    (optimize-char-table table)
+    (set-char-table-parent table char-width-table)
+    (setq char-width-table table)))
