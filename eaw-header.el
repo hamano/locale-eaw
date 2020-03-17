63ff8254d51718ea7b933a20c66c4cd8c12abe42
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-03-17 09:57:00 +0900

    add eaw-header.el

---
+++ eaw-header.el
@@ -0,0 +1,8 @@
+;;; eaw.el --- Fix east asian ambiguous width issue for emacs
+;; Author: HAMANO Tsukasa <code@cuspy.org>
+;; URL: https://github.com/hamano/locale-eaw
+;; Version: 12
+;; MIT License
+
+;;; Code:
+
